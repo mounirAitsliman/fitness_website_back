ffcc508d293cf4851ad218ef14f02c3322eea0df
Mounir Ait Sliman <105859627+mounirAitsliman@users.noreply.github.com>
2023-06-09 17:24:10 +0100

    separate my frontend and backend

--- BotTraining/app.py
+++ BotTraining/app.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 from chat import get_response
 
 
 
-app = Flask(__name__ , template_folder='templates')
+app = Flask(__name__)
 
-
-
-
-@app.get("/")
-def index_get():
-    return render_template("base.html")
+CORS(app)
 
 
 @app.route("/login")
