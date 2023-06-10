0e3869e8ec71f347a735162b04a07e6a0740ddd3
Mounir Ait Sliman <105859627+mounirAitsliman@users.noreply.github.com>
2023-06-10 15:14:41 +0100

    ziad's update

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from chat import get_response
 
 
 
-app = Flask(__name__)
-
+app = Flask(__name__ , template_folder='templates')
 CORS(app)
+
+
+
+
+""" 
+@app.get("/")
+def index_get():
+    return render_template("base.html") """
 
 
 @app.route("/login")
